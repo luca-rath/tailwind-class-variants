d5e522a12d4f396da0f7f4fa215d8f3d01ff714c
Luca Rath-Heel <luca@sulu.io>
2021-12-03 16:22:00 +0100

    Add plugin

---
+++ README.md
@@ -0,0 +1,68 @@
+# tailwind-class-variants
+
+A tailwindcss plugin that makes it possible to add conditional styling to an element only if that element has a specific class.
+
+## Installation
+
+> Note that `tailwind-class-variants` is designed for Tailwind CSS v2.2 with JIT mode enabled.
+
+Install the plugin from npm:
+
+```sh
+# Using npm
+npm install tailwind-class-variants
+
+# Using Yarn
+yarn add tailwind-class-variants
+```
+
+Then add the plugin to your `tailwind.config.js` file:
+
+```js
+// tailwind.config.js
+module.exports = {
+  mode: 'jit',
+  // ...
+  plugins: [
+    require('tailwind-class-variants')({
+      classes: ['is-active'],
+    }),
+    // ...
+  ],
+}
+```
+
+This will then generate the following variants:
+
+- `is-active`
+- `group-is-active`
+- `peer-is-active`
+
+## Basic usage
+
+```html
+<ul id="list">
+    <li class="is-active:font-bold is-active">Foo</li>
+    <li class="is-active:font-bold">Bar</li>
+    <li class="is-active:font-bold">Baz</li>
+</ul>
+
+<script>
+    const list = document.querySelector('#list');
+    const items = list.querySelectorAll('li');
+
+    const handleClick = (event) => {
+        items.forEach((item) => {
+            if (item === event.currentTarget) {
+                item.classList.add('is-active');
+            } else {
+                item.classList.remove('is-active');
+            }
+        })
+    }
+
+    items.forEach((item) => {
+        item.addEventListener('click', handleClick);
+    });
+</script>
+```
---
+++ src/index.js
@@ -0,0 +1,77 @@
+const plugin = require('tailwindcss/plugin');
+const prefixSelector = require('tailwindcss/lib/util/prefixSelector');
+const {
+    applyPseudoToMarker,
+    updateAllClasses,
+    transformAllSelectors,
+    transformAllClasses,
+} = require('tailwindcss/lib/util/pluginUtils');
+
+module.exports = plugin.withOptions(({ classes = [] } = {}) => {
+    return function ({ addVariant, config }) {
+        let pseudoVariants = classes.map((x) => [x, `is(.${x})`]);
+
+        // The following code is copied from https://github.com/tailwindlabs/tailwindcss/blob/v2.2.19/src/jit/corePlugins.js#L149-L209
+        for (let variant of pseudoVariants) {
+            let [variantName, state] = Array.isArray(variant) ? variant : [variant, variant]
+
+            addVariant(
+                variantName,
+                transformAllClasses((className, { withPseudo }) => {
+                    return withPseudo(`${variantName}${config('separator')}${className}`, `:${state}`)
+                })
+            )
+        }
+
+        let groupMarker = prefixSelector(config('prefix'), '.group')
+        for (let variant of pseudoVariants) {
+            let [variantName, state] = Array.isArray(variant) ? variant : [variant, variant]
+            let groupVariantName = `group-${variantName}`
+
+            addVariant(
+                groupVariantName,
+                transformAllSelectors((selector) => {
+                    let variantSelector = updateAllClasses(selector, (className) => {
+                        if (`.${className}` === groupMarker) return className
+                        return `${groupVariantName}${config('separator')}${className}`
+                    })
+
+                    if (variantSelector === selector) {
+                        return null
+                    }
+
+                    return applyPseudoToMarker(
+                        variantSelector,
+                        groupMarker,
+                        state,
+                        (marker, selector) => `${marker} ${selector}`
+                    )
+                })
+            )
+        }
+
+        let peerMarker = prefixSelector(config('prefix'), '.peer')
+        for (let variant of pseudoVariants) {
+            let [variantName, state] = Array.isArray(variant) ? variant : [variant, variant]
+            let peerVariantName = `peer-${variantName}`
+
+            addVariant(
+                peerVariantName,
+                transformAllSelectors((selector) => {
+                    let variantSelector = updateAllClasses(selector, (className) => {
+                        if (`.${className}` === peerMarker) return className
+                        return `${peerVariantName}${config('separator')}${className}`
+                    })
+
+                    if (variantSelector === selector) {
+                        return null
+                    }
+
+                    return applyPseudoToMarker(variantSelector, peerMarker, state, (marker, selector) =>
+                        selector.trim().startsWith('~') ? `${marker}${selector}` : `${marker} ~ ${selector}`
+                    )
+                })
+            )
+        }
+    };
+});
